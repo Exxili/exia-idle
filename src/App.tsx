1785837031e6f0a46f392a1495f3c6ad13a3f3b0
Exxili <andy@exxili.com>
2024-09-09 11:34:18 +0100

    added rnd for windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import { Rnd } from "react-rnd";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="bg-blue-500 w-[100vw] h-[100vh] flex items-center justify-center">
+      <Rnd
+        default={{
+          x: window.innerWidth / 2 - 160,
+          y: window.innerHeight / 2 - 100,
+          width: 320,
+          height: 200,
+        }}
+        className="bg-red-500"
+      >
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </Rnd>
+    </div>
   );
 }
 
